8e5e750a5270fbde1cd44a5435da6ee50c692355
austinh2001 <austinh2001@gmail.com>
2025-11-19 08:42:09 -0500

    Update action.lua

--- action.lua
+++ action.lua
@@ -194,6 +194,9 @@ local function transplantWorld(src, dest)
 
     inventory_controller.suckFromSlot(sides.up, 1, 1)  -- Retrieve the wand
 
+    inventory_controller.equip()
+    robot.select(selectedSlot)
+
     gps.goWorld(src)
     robot.useDown(sides.down, true)
 
