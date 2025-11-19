989ac3227635cc17d4f2d548b642e8b44d0e0661
austinh2001 <austinh2001@gmail.com>
2025-11-19 08:51:53 -0500

    Update action.lua

--- action.lua
+++ action.lua
@@ -208,7 +208,7 @@ local function transplantWorld(src, dest)
     robot.select(robot.inventorySize() + config.wandSlot)
     inventory_controller.equip()
 
-    robot.select(1)
+    robot.select(selectedSlot)
 end
 
 
